a6a00ad0415aa366da42a08fc00ed0a4157b66dc
Harish Nayak <harishnayak2141@gmail.com>
2026-07-15 21:08:40 +0530

    Time: 0 ms (100%), Space: 42.6 MB (39.85%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        // oddsum = n*n
+        // evensum = n(n+1)
+        // gcd(n*n,n(n+1)) = n(n,n+1) = 1
+        return n;
+        
+    }
+}
