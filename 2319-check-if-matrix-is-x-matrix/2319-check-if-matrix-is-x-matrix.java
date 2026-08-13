e1dd8b3db40305a9900851af30dd23cc1aac81ba
Harish Nayak <harishnayak2141@gmail.com>
2026-08-13 14:36:25 +0530

    Time: 3 ms (23.4%), Space: 47.4 MB (15.81%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
@@ -2,24 +2,23 @@ class Solution {
     public boolean checkXMatrix(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        for(int i = 0; i < m*n; i++){
-            int row = i / n;
-            int col = i % n;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
             //main diagonal ke liye top left se bottom right k liye
-            if(row == col && grid[row][col] == 0){
+            if(i == j && grid[i][j] == 0){
                 return false;
                 
             }
             // anti diagonal top right se bottom left k liye
-            if(col == n - 1 - row){
-                if(grid[row][col] == 0) return false;
+            if(j == n - 1 - i){
+                if(grid[i][j] == 0) return false;
             }
             // jo diagonal nhi he unke liye
-            if(row != col && col != n - 1 - row){
-             if(grid[row][col] != 0)
-             return false;
+            if(i != j && j != n - 1 - i){
+             if(grid[i][j] != 0)
+              return false;
             }
-            
+        }
 
         }
             return true;
