2dc6c7476ba7cf6acb9791ed596b7fd56c0ffbd7
Harish Nayak <harishnayak2141@gmail.com>
2026-08-15 00:03:51 +0530

    Time: 3 ms (86.31%), Space: 52.5 MB (5.89%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int candies[] = new int[ratings.length];
+        Arrays.fill(candies,1);
+        //left to right
+        for(int i = 1; i < ratings.length ; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        //right to  left
+        for(int i = ratings.length - 2; i >= 0 ; i-- ){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i],candies[i+1] + 1);
+            }
+        }
+        int sum = 0;
+        for(int num : candies){
+            sum += num;
+        }
+        return sum;
+        
+    }
+}
