96df9209c59eba6807efbe4aa2ce7a3109118992
Harish Nayak <harishnayak2141@gmail.com>
2026-08-21 19:17:15 +0530

    Time: 30 ms (91.85%), Space: 48.7 MB (32.9%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : candyType){
+            set.add(num);
+        }
+        int ans = candyType.length/2;
+        return Math.min(set.size(),ans);
+        
+    }
+}
